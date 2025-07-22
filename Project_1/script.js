ba9e53922cb8c4668699b8afe0ca0fbd154781c0
Abdul Mohaiman <abdulmohaiman333@gmail.com>
2025-07-22 03:25:06 -0700

    new practice

--- Project_1/script.js
+++ Project_1/script.js
@@ -1,46 +1,53 @@
-let userArr = [];
-let roll = [];
+// let userArr = [];
+// let roll = [];
 
-document.querySelector("#mainForm").addEventListener("submit", (e)=>{
-    e.preventDefault()
-    let rollNo = document.querySelector("#i0").value
-    let name = document.querySelector("#i1").value
-    let fatherName = document.querySelector("#i2").value
-    let email = document.querySelector("#i3").value
+// document.querySelector("#mainForm").addEventListener("submit", (e)=>{
+//     e.preventDefault()
+//     let rollNo = document.querySelector("#i0").value
+//     let name = document.querySelector("#i1").value
+//     let fatherName = document.querySelector("#i2").value
+//     let email = document.querySelector("#i3").value
 
-    if(roll.includes(rollNo)){
-        alert("Roll Number already exists.")
-        return;
-    }
-    roll.push(rollNo)
+//     if(roll.includes(rollNo)){
+//         alert("Roll Number already exists.")
+//         return;
+//     }
+//     roll.push(rollNo)
 
-    let userObj = {
-        rollNumber: rollNo,
-        username: name,
-        userFather: fatherName,
-        userEmail: email
-    }
+//     let userObj = {
+//         rollNumber: rollNo,
+//         username: name,
+//         userFather: fatherName,
+//         userEmail: email
+//     }
 
 
-    function addNewRow(userObj){
-        let row = document.createElement("tr")
-        row.innerHTML = `<td>${userObj.rollNumber}</td>
-                        <td>${userObj.username}</td>
-                        <td>${userObj.userFather}</td>
-                        <td>${userObj.userEmail}</td>
-                        <button id="delBtn">Delete</button>`
-        return row
-    }
+//     function addNewRow(userObj){
+//         let row = document.createElement("tr")
+//         row.innerHTML = `<td>${userObj.rollNumber}</td>
+//                         <td>${userObj.username}</td>
+//                         <td>${userObj.userFather}</td>
+//                         <td>${userObj.userEmail}</td>
+//                         <button id="delBtn">Delete</button>`
+//         return row
+//     }
 
     
-    function addRowtoTable(userObj){
-        let table = document.querySelector("#mainTable tbody")
-        let tr = addNewRow(userObj)
-        table.appendChild(tr)
-    }
+//     function addRowtoTable(userObj){
+//         let table = document.querySelector("#mainTable tbody")
+//         let tr = addNewRow(userObj)
+//         table.appendChild(tr)
+//     }
     
-    addRowtoTable(userObj);
-    userArr.push(userObj);
-    console.log(userArr);
+//     addRowtoTable(userObj);
+//     userArr.push(userObj);
+//     console.log(userArr);
 
-})
+// })
+
+
+
+
+let x = 5;
+let y = x++ + ++x + x++;
+console.log(y)
